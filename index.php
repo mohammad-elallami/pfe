4bb7af19389a7453fa21ae559fc1913081ac4557
mohammad-elallami <166083611+mohammad-elallami@users.noreply.github.com>
2024-04-05 11:59:46 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecom pfe</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Ecom pfe</h1>
+    </header>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Category</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+		<a href="#">login</a>
+        <a href="#">register</a>
+    </nav>
+    <section id="products">
+    
+    </section>
+    <footer>
+        &copy; 2024 Ecom pfe. All rights reserved.
+    </footer>
+
+</body>
+</html>
+
+
+
